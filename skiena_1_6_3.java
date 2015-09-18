bf275579a78604dd936c1a8d37f11a0f992a1f51
iceguyvn <nguyen_my@yahoo.com>
2015-09-18 16:27:15 -0700

    Skiena 1.6.3 Java, not-so-precise float submission

---
+++ skiena_1_6_3.java
@@ -0,0 +1,90 @@
+import java.util.*;
+
+class skiena_1_6_3
+{
+  static List<Expenses> input()
+  {
+    List<Expenses> list = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    while (true)
+    {
+      // read in the number of students on this Expenses
+      int count = scanner.nextInt();
+
+      // end of input
+      if (count == 0)
+        break;
+      else
+      {
+        // read in the expense of each student in this Expenses
+        Expenses expenses = new Expenses();
+        for (int i = 0; i < count; i++)
+        {
+          float expense = scanner.nextFloat();
+          expenses.data.add(expense);
+        }
+
+        list.add(expenses);
+      }
+    }
+    scanner.close();
+    return list;
+  }
+
+  static void output(List<Expenses> list)
+  {
+    for (Expenses expenses : list)
+    {
+      // sort all the expenses, to facilitate calculating the average and the
+      // exchange amount per student
+      expenses.sort();
+
+      // print output in the format required
+      System.out.printf("$%.2f\n", expenses.exchange());
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    List<Expenses> list = input();
+    output(list);
+  }
+}
+
+class Expenses
+{
+  public List<Float> data = new ArrayList<>();
+
+  public void sort()
+  {
+    Collections.sort(data);
+  }
+
+  public String toString()
+  {
+    StringBuilder builder = new StringBuilder();
+    for (float expense : data)
+      builder.append(expense).append(" ");
+    builder.append("\n");
+
+    return builder.toString();
+  }
+
+  float average()
+  {
+    float total = 0.0f;
+    for (float expense : data)
+      total += expense;
+
+    return total / data.size();
+  }
+
+  public float exchange()
+  {
+    float total = 0.0f;
+    float avg = average();
+    for (int i = 0; i < data.size() && data.get(i) < avg; i++)
+      total += avg - data.get(i);
+    return total;
+  }
+}
