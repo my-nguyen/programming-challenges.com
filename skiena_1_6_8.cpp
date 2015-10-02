018e6e93abbb04a2a066d1078813361b3400a1d4
iceguyvn <nguyen_my@yahoo.com>
2015-10-01 20:49:18 -0700

    Skiena 1.6.8 C++ complete refactored

--- skiena_1_6_8.cpp
+++ skiena_1_6_8.cpp
@@ -7,36 +7,36 @@ using namespace std;
 using namespace boost;
 
 // this class represents a list of integer indices. its main purpose is for the
-// convenience of printing by way of method toString()
+// convenience of printing via operator string()
 class Indices
 {
 public:
-  vector<int> data;
+  vector<int> list;
 
   operator string()
   {
     string builder;
-    for (vector<int>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<int>::iterator it = list.begin(); it != list.end(); it++)
       builder.append(" ").append(to_string(*it+1));
     return builder;
   }
 };
 
+// this class represents a ballot, which is a vector of int votes
 class Ballot
 {
 public:
-  vector<int> choices;
+  vector<int> votes;
 
-  // this constructor initializes the ballot with integers extracted from a string
+  // this constructor initializes the ballot with integers (votes) extracted
+  // from a string
   Ballot(string& line)
   {
     tokenizer<char_separator<char> > tokens(line);
-    for (tokenizer<char_separator<char> >::iterator it = tokens.begin();
-      it != tokens.end();
-      it++)
+    for (tokenizer<char_separator<char> >::iterator it = tokens.begin(); it != tokens.end(); it++)
     {
-      int choice = stoi(*it);
-      choices.push_back(choice);
+      int vote = stoi(*it);
+      votes.push_back(vote);
     }
   }
 
@@ -45,7 +45,7 @@ public:
   {
     string builder;
     builder.append("[");
-    for (vector<int>::iterator it = choices.begin(); it != choices.end(); it++)
+    for (vector<int>::iterator it = votes.begin(); it != votes.end(); it++)
       builder.append(to_string(*it)).append(" ");
     builder.pop_back();
     builder.append("]");
@@ -56,11 +56,12 @@ public:
   static Indices indices;
   // unary function (that accepts an element of vector<int> as argument and
   // returns bool) as required by std::remove_if(). this function must be static
-  // so std::remove_if() can call it.
+  // so std::remove_if() can call it. this function returns whether an int item
+  // is found in a vector<int> (indices)
   // class Ballot
   static bool find_loser(int item)
   {
-    bool found = (find(indices.data.begin(), indices.data.end(), item-1) != indices.data.end());
+    bool found = (find(indices.list.begin(), indices.list.end(), item-1) != indices.list.end());
     return found;
   }
 
@@ -74,75 +75,89 @@ public:
     indices = losers_indices;
     // use the remove-erase idiom to remove an element from vector while
     // iterating over it.
-    choices.erase(remove_if(choices.begin(), choices.end(), find_loser), choices.end());
+    votes.erase(remove_if(votes.begin(), votes.end(), find_loser), votes.end());
   }
 };
 Indices Ballot::indices;
 
+// this class is comprised of vector<Ballots*> and not vector<Ballot>. this
+// class serves as a collection of pointers to a subset of ballots within the
+// master list of ballots. this class is used by class Candidate and by output()
+// in order to display/remove votes from individual ballots without removing
+// the ballots from the master list. this class is born out of necessity, since
+// C++ distinguishes between an object and its pointer, whereas Java doesn't and
+// which spares the creation of this class.
 class BallotPointers
 {
 public:
-  vector<Ballot*> data;
+  vector<Ballot*> list;
 
   operator string()
   {
     string builder;
-    for (vector<Ballot*>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Ballot*>::iterator it = list.begin(); it != list.end(); it++)
       builder.append(" ").append(**it);
     return builder;
   }
 
+  // this method is similar in functionality to Ballots::match()
   // class BallotPointers
-  BallotPointers match(int choice)
+  BallotPointers match(int vote)
   {
     BallotPointers result;
-    for (vector<Ballot*>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Ballot*>::iterator it = list.begin(); it != list.end(); it++)
     {
-      if ((*it)->choices[0] == choice)
+      if ((*it)->votes[0] == vote)
         // retain the address (within vector<Ballot>) of the matching ballot
-        result.data.push_back(*it);
+        result.list.push_back(*it);
     }
     return result;
   }
 };
 
-class BallotList
+// this class represents the master list of all ballots of the current poll.
+// each ballot is comprised of many int votes. the votes may be removed from
+// ballots, but no ballots may be removed from this master list.
+class Ballots
 {
 public:
-  vector<Ballot> data;
+  vector<Ballot> list;
 
   operator string()
   {
     string builder;
-    for (vector<Ballot>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Ballot>::iterator it = list.begin(); it != list.end(); it++)
       builder.append(" ").append(*it);
     return builder;
   }
 
-  // class BallotList
+  // class Ballots
   void remove_votes(Indices& losers_indices)
   {
-    for (vector<Ballot>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Ballot>::iterator it = list.begin(); it != list.end(); it++)
       it->remove_votes(losers_indices);
   }
 
   // this method searches through a master list of ballots for those whose first
-  // choice match the current candidate and returns the list of matching ballots.
-  // the master list of ballots remains unmodified.
-  // class BallotList
-  BallotPointers match(int choice)
+  // vote match the current candidate and returns a list of pointers to matching
+  // ballots. the master list of ballots remains unmodified.
+  // class Ballots
+  BallotPointers match(int candidate)
   {
     BallotPointers result;
-    for (vector<Ballot>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Ballot>::iterator it = list.begin(); it != list.end(); it++)
     {
-      if (it->choices[0] == choice)
+      if (it->votes[0] == candidate)
         // retain the address (within vector<Ballot>) of the matching ballot
-        result.data.push_back(&*it);
+        result.list.push_back(&*it);
     }
     return result;
   }
 };
 
+// this class represents a candidate, which comprises of a name and a
+// vector<Ballot*>, which allows individual candidates to modify the contents of
+// Ballot's residing in the master list of ballots.
 class Candidate
 {
 public:
@@ -156,16 +171,18 @@ public:
 
   int size()
   {
-    return ballots.data.size();
+    return ballots.list.size();
   }
 
+  // this method adds new ballot pointers to the current vector<Ballot*> of this
+  // candidate
   // class Candidate
   void add_ballots(BallotPointers new_ballots)
   {
-    if (ballots.data.size() != 0 && new_ballots.data.size() != 0)
+    if (ballots.list.size() != 0 && new_ballots.list.size() != 0)
     {
-      for (vector<Ballot*>::iterator it = new_ballots.data.begin(); it != new_ballots.data.end(); it++)
-        ballots.data.push_back(*it);
+      for (vector<Ballot*>::iterator it = new_ballots.list.begin(); it != new_ballots.list.end(); it++)
+        ballots.list.push_back(*it);
     }
   }
 
@@ -174,16 +191,21 @@ public:
   {
     string builder;
     builder.append("<").append(name).append("> ");
-    builder.append(to_string(ballots.data.size())).append(" ballots\n");
+    builder.append(to_string(ballots.list.size())).append(" ballots\n");
     builder.append(ballots);
     return builder;
   }
 };
 
+// this class comprises of a vector<Candidate*>. it's created via Candidates::
+// min_vote_candidates(), which is a subset of all candidates. it serves to
+// identify the losing candidates as well as their voting numbers (indices), and
+// to collect the ballots associate with these candidates so that their votes
+// can be removed and re-counted.
 class CandidatePointers
 {
 public:
-  vector<Candidate*> data;
+  vector<Candidate*> list;
   Candidate* base;
 
   CandidatePointers(Candidate* address)
@@ -191,12 +213,14 @@ public:
     base = address;
   }
 
+  // this method returns a list of indices associated with these candidates. the
+  // indices are also the candidates' voting numbers/rankings.
   // class CandidatePointers
   Indices indices()
   {
     Indices result;
-    for (vector<Candidate*>::iterator it = data.begin(); it != data.end(); it++)
-      result.data.push_back(*it - base);
+    for (vector<Candidate*>::iterator it = list.begin(); it != list.end(); it++)
+      result.list.push_back(*it - base);
     return result;
   }
 
@@ -204,7 +228,7 @@ public:
   operator string()
   {
     string builder;
-    for (vector<Candidate*>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Candidate*>::iterator it = list.begin(); it != list.end(); it++)
       builder.append(**it);
     return builder;
   }
@@ -214,9 +238,9 @@ public:
   BallotPointers all_ballots()
   {
     BallotPointers result;
-    for (vector<Candidate*>::iterator it = data.begin(); it != data.end(); it++)
-      for (vector<Ballot*>::iterator jt = (*it)->ballots.data.begin(); jt != (*it)->ballots.data.end(); jt++)
-        result.data.push_back(*jt);
+    for (vector<Candidate*>::iterator it = list.begin(); it != list.end(); it++)
+      for (vector<Ballot*>::iterator jt = (*it)->ballots.list.begin(); jt != (*it)->ballots.list.end(); jt++)
+        result.list.push_back(*jt);
 
     return result;
   }
@@ -225,48 +249,49 @@ public:
   // class CandidatePointers
   void eliminate()
   {
-    for (vector<Candidate*>::iterator it = data.begin(); it != data.end(); it++)
-      (*it)->ballots.data.clear();
+    for (vector<Candidate*>::iterator it = list.begin(); it != list.end(); it++)
+      (*it)->ballots.list.clear();
   }
 };
 
-class CandidateList
+// this class represents a list of all candidates
+class Candidates
 {
 public:
-  vector<Candidate> data;
+  vector<Candidate> list;
 
   operator string()
   {
     string builder;
-    for (vector<Candidate>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Candidate>::iterator it = list.begin(); it != list.end(); it++)
       builder.append(*it);
     return builder;
   }
 
   // this method finds a candidate whose total ballot count is more than 50% of
   // the total ballot count
-  // class CandidateList
+  // class Candidates
   Candidate* find_winner(int ballot_count)
   {
-    for (int i = 0; i < data.size(); i++)
+    for (int i = 0; i < list.size(); i++)
     {
-      Candidate* candidate = &data[i];
-      // cout << "total ballots: " << ballot_count << ", candidate: " << (i+1) << ", ballot count: " << candidate->ballots.data.size() << endl;
+      Candidate* candidate = &list[i];
+      // cout << "total ballots: " << ballot_count << ", candidate: " << (i+1) << ", ballot count: " << candidate->ballots.list.size() << endl;
       // look for candidate with ballot count more than 50% of the total votes
-      if (candidate->ballots.data.size() / (float)ballot_count >= .5)
+      if (candidate->ballots.list.size() / (float)ballot_count >= .5)
         return candidate;
     }
     return NULL;
   }
 
   // this method returns the lowest number of votes among all candidates
-  // class CandidateList
+  // class Candidates
   int min_vote_count()
   {
     int min_vote_count = numeric_limits<int>::max();
-    for (vector<Candidate>::iterator it = data.begin(); it != data.end(); it++)
+    for (vector<Candidate>::iterator it = list.begin(); it != list.end(); it++)
     {
-      int vote_count = it->ballots.data.size();
+      int vote_count = it->ballots.list.size();
       if (vote_count != 0)
         if (vote_count < min_vote_count)
           min_vote_count = vote_count;
@@ -276,42 +301,32 @@ public:
     return min_vote_count;
   }
 
-  // this method returns a list of index to all Candidate's whose ballot count
+  // this method returns a list of pointers to all Candidate's whose ballot count
   // equals min_vote_count, aka all losers. the returned list will be used to:
   // (1) collect all ballots of all losers (so the losers can be removed from
   // these ballots); and (2) eliminate the losers so they won't feature in
-  // future rounds of min_vote_count(). in a previous implementation, a list of
-  // Candidate's was returned to perform steps (1) and (2) above. however, after
-  // collecting all ballots of all losers, the first element of each ballot was
-  // simply removed, which yielded incorrect result. for example, in one round,
-  // the losers tie for lowest votes of 4 have their index at 1, 5, 8, 11. These
-  // 4 losers have a combined list of 16 ballots, some of which are [5, 11, 8, 9],
-  // [5, 1, 9, 6], [8, 5, 13, 4], [8, 5, 3, 2]. removing the first element from
-  // each ballots will result: [11, 8, 9], [1, 9, 6], [5, 13, 4], [5, 3, 2]
-  // which is incorrect. in this implementation, a list of indices insteall will
-  // additionally serve to keep removing the losers from the 16 ballots (as
-  // opposed to just removing the first element), so that the ballots will end
-  // up with correct result: [9], [9, 6], [13, 4], [3, 2].
-  // class CandidateList
+  // future rounds of min_vote_count().
+  // class Candidates
   CandidatePointers min_vote_candidates(int min_vote_count)
   {
-    CandidatePointers candidates(&data[0]);
-    for (vector<Candidate>::iterator it = data.begin(); it != data.end(); it++)
+    CandidatePointers candidates(&list[0]);
+    for (vector<Candidate>::iterator it = list.begin(); it != list.end(); it++)
     {
       // a candidate whose total vote count equals the minimum vote count
-      if (it->ballots.data.size() == min_vote_count)
-        candidates.data.push_back(&*it);
+      if (it->ballots.list.size() == min_vote_count)
+        candidates.list.push_back(&*it);
     }
     return candidates;
   }
 
-  // this method adds a new list of ballots into the current list of candidates
-  // class CandidateList
+  // this method re-circulates the eliminated candidates' ballots by adding
+  // those ballots into the list of active candidates
+  // class Candidates
   void recount_ballots(BallotPointers& ballots)
   {
-    for (int i = 0; i < data.size(); i++)
+    for (int i = 0; i < list.size(); i++)
     {
-      Candidate* candidate = &data[i];
+      Candidate* candidate = &list[i];
       // add all ballots whose first vote is the current candidate to the
       // candidate's list of current ballots
       candidate->add_ballots(ballots.match(i + 1));
@@ -321,7 +336,7 @@ public:
   int size()
   {
     int size = 0;
-    for (Candidate candidate : data)
+    for (Candidate candidate : list)
       size += candidate.size();
     return size;
   }
@@ -329,16 +344,16 @@ public:
   // this method goes thru the current list of candidates to find those whose
   // ranking (position/index) matches the indices, collects and returns those
   // candidates
-  // class CandidateList
-  CandidateList losers(Indices& losers_indices)
+  // class Candidates
+  Candidates losers(Indices& losers_indices)
   {
-    CandidateList losers;
-    for (int i = 0; i < data.size(); i++)
-      for (vector<int>::iterator it = losers_indices.data.begin();
-           it != losers_indices.data.end();
+    Candidates losers;
+    for (int i = 0; i < list.size(); i++)
+      for (vector<int>::iterator it = losers_indices.list.begin();
+           it != losers_indices.list.end();
            it++)
         if (i+1 == *it)
-          losers.data.push_back(data[i]);
+          losers.list.push_back(list[i]);
     return losers;
   }
 };
@@ -347,7 +362,7 @@ class Poll
 {
 public:
   vector<string> names;
-  BallotList ballots;
+  Ballots ballots;
 
   operator string()
   {
@@ -356,7 +371,7 @@ public:
     for (vector<string>::iterator it = names.begin(); it != names.end(); it++)
       builder.append(" <").append(*it).append(">\n");
 
-    builder.append(to_string(ballots.data.size())).append(" ballots:\n");
+    builder.append(to_string(ballots.list.size())).append(" ballots:\n");
     builder.append(ballots);
     return builder;
   }
@@ -392,7 +407,7 @@ vector<Poll> input()
         break;
       else
         // collect all ballots
-        poll.ballots.data.push_back(Ballot(line));
+        poll.ballots.list.push_back(Ballot(line));
     }
 
     list.push_back(poll);
@@ -407,8 +422,8 @@ void output(vector<Poll>& list)
   for (vector<Poll>::iterator it = list.begin(); it != list.end(); it++)
   {
     // cout << "POLL\n" + it->operator string();
-    CandidateList candidates;
-    int ballot_count = it->ballots.data.size();
+    Candidates all_candidates;
+    int ballot_count = it->ballots.list.size();
 
     // for each name in the current poll
     for (int j = 0; j < it->names.size(); j++)
@@ -416,43 +431,43 @@ void output(vector<Poll>& list)
       string name = it->names[j];
       // create a candidate with such a name
       Candidate candidate(name);
-      // collect those ballots whose first choice match the current candidate,
+      // collect those ballots whose first vote match the current candidate,
       // based on the candidate's current position (index). the master list of
       // ballots remains unchanged.
       candidate.ballots = it->ballots.match(j + 1);
 
       // cout << "Candidate " << (j+1) << ": " + candidate.operator string();
       // save the candidate
-      candidates.data.push_back(candidate);
+      all_candidates.list.push_back(candidate);
     }
 
     // keep looking for a winner, one with more than 50% of the vote
-    Candidate* winner = candidates.find_winner(ballot_count);
+    Candidate* winner = all_candidates.find_winner(ballot_count);
     while (winner == NULL)
     {
       // begin the process of elimination: start by looking for the lowest
       // number of votes received by each candidate
-      int min_vote_count = candidates.min_vote_count();
+      int min_vote_count = all_candidates.min_vote_count();
       // obtain the position (index) of the candidates with the lowest number
       // of votes.
       // collect all actual candidates with the lowest number of votes.
-      CandidatePointers losing_candidates = candidates.min_vote_candidates(min_vote_count);
+      CandidatePointers losing_candidates = all_candidates.min_vote_candidates(min_vote_count);
       Indices losers_indices = losing_candidates.indices();
       // cout << "Losers' indices:" << losers_indices.operator string() << endl;
       // cout << "Losers:" << endl << losing_candidates.operator string();
       // collect all ballots belonging to the losing candidates
       BallotPointers losers_ballots = losing_candidates.all_ballots();
       // cout << "All losers' ballots:\n" << losers_ballots.operator string();
-      // eliminate these candidates from all ballots
+      // eliminate the losing candidates from the master list of ballots
       it->ballots.remove_votes(losers_indices);
       // cout << "All losers' ballots, refreshed:\n" << losers_ballots.operator string();
       // eliminate these candidates from the voting poll
       losing_candidates.eliminate();
       // recount the ballots in favor of their highest-ranked non-eliminated
       // candidate
-      candidates.recount_ballots(losers_ballots);
+      all_candidates.recount_ballots(losers_ballots);
       // keep looking for a winner
-      winner = candidates.find_winner(ballot_count);
+      winner = all_candidates.find_winner(ballot_count);
     }
     cout << winner->name << endl;
   }
