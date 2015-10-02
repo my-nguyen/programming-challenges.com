c98eeb6052d255fb28e02bb365102d89d6da327c
iceguyvn <nguyen_my@yahoo.com>
2015-10-02 15:19:51 -0700

    Skiena 2.8.1 C++ complete refactored

--- skiena_2_8_1.cpp
+++ skiena_2_8_1.cpp
@@ -1,234 +1,141 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <cstring> // strcpy(), strtok()
-#include <cstdlib> // atoi()
-#include "defs.h"
+#include <cmath>  // abs()
 using namespace std;
 
-
-int _debug = DEBUG_OFF;
-
-
-class Input
+// this class represents a list of integers
+class Numbers
 {
 public:
-    vector<int> _list;
+  vector<int> list;
 
-    Input();
-    Input(int size);
-    Input& operator=(const Input& that);
-   ~Input();
-    void print();
+  operator string()
+  {
+    string builder;
+    builder.append(to_string(list.size()));
+    for (int i = 0; i < list.size(); i++)
+      builder.append(" ").append(to_string(list[i]));
+    builder.append("\n");
+    return builder;
+  }
 };
 
-
-Input::Input()
-{
-}
-
-
-Input::Input(int size)
-{
-    _list.resize(size);
-}
-
-
-Input& Input::operator=(const Input& that)
-{
-    _list = that._list;
-    return *this;
-}
-
-
-Input::~Input()
-{
-}
-
-
-void Input::print()
-{
-    cout << _list.size();
-    for (int i = 0; i < _list.size(); i++)
-        cout << " " << _list[i];
-    cout << endl;
-}
-
-
-class InputList
+class Flag
 {
 public:
-    vector<Input> _list;
+  bool flag;
 
-    InputList() {}
-   ~InputList() {}
+  Flag(bool fl)
+  {
+    flag = fl;
+  }
+
+  operator string()
+  {
+    return (flag ? "true" : "false");
+  }
 };
 
-
-class Difference
+// this class represents a list of boolean flags
+class Flags
 {
 public:
-    vector<bool> _list;
+  vector<Flag> list;
 
+  Flags(int size)
+  {
+    for (int i = 0; i < size; i++)
+      list.push_back(Flag(false));
+  }
 
-    Difference(int size);
-    void print();
-    bool scan();
+  operator string()
+  {
+    string builder;
+    builder.append("Flags entries:\n");
+    for (int i = 0; i < list.size(); i++)
+      builder.append(list[i].operator string()).append(" ");
+    builder.append("\n");
+    return builder;
+  }
+
+  // this method scans the array for all unmarked flags and returns true if all
+  // flags are marked, and false if any flag is unmarked. note that it goes thru
+  // the entire array looking for all unmarked flags and it doesn't just return
+  // upon the first unmarked flag
+  bool scan()
+  {
+    bool result = true;
+    for (int i = 1; i < list.size(); i++)
+      if (list[i].flag == false)
+      {
+        // cout << "Entry " + i + " unmarked" << endl;
+        result = false;
+      }
+
+    return result;
+  }
 };
 
-
-Difference::Difference(int size)
+// this method determines whether a sequence of integers is a jolly jumper
+bool is_jolly(Numbers numbers)
 {
-    _list.resize(size, false);
-}
-
-
-void Difference::print()
-{
-    cout << "Difference entries:" << endl;
-    for (int i = 1; i < _list.size(); i++)
-        cout << _list[i] << " ";
-    cout << endl;
-}
-
-
-// scan the visited array to see if any number was not represented
-bool Difference::scan()
-{
-    for (int i = 1; i < _list.size(); i++)
-        if (_list[i] == false)
-        {
-            if (_debug >= DEBUG_COPIOUS)
-                cout << "Entry " << i << " not represented" << endl;
-            return false;
-        }
-
+  // a sequence having only one integer is a jolly jumper
+  if (numbers.list.size() == 1)
     return true;
+  else
+  {
+    // create an array where each entry (between 1 and n-1) indicates whether
+    // it has been represented in the Numbers sequence
+    Flags flags(numbers.list.size());
+
+    // for each absolute difference, mark it in the visited array
+    for (int i = 0; i < numbers.list.size()-1; i++)
+    {
+      int difference = abs(numbers.list[i]-numbers.list[i+1]);
+      if (difference <= numbers.list.size())
+      {
+        flags.list[difference] = true;
+        // cout << "Marking " << difference << endl;
+      }
+      else
+      {
+        // cout << difference << " is out of bound" << endl;
+      }
+    }
+
+    return flags.scan();
+  }
 }
 
-
-bool is_jolly(Input* input)
+vector<Numbers> input()
 {
-    // a sequence having only one integer is a jolly jumper
-    if (input->_list.size() == 1)
-        return true;
-    else
+  vector<Numbers> list;
+  // read the first number, which is the size of how many more integers to follow
+  int count;
+  while (cin >> count)
+  {
+    // read the sequence of integers that follow the size
+    Numbers current;
+    for (int j = 0; j < count; j++)
     {
-        // create an array whose entries (between 1 and n-1) signify whether
-        // such a value has been represented in the input sequence
-        Difference diff(input->_list.size());
-
-
-        // for each absolute difference, mark it in the visited array
-        for (int i = 0; i < input->_list.size()-1; i++)
-        {
-            int abs_diff = abs(input->_list[i]-input->_list[i+1]);
-            if (_debug >= DEBUG_COPIOUS)
-                cout << "Absolute difference between " << input->_list[i]
-                     << " and " << input->_list[i+1] << " is " << abs_diff
-                     << endl;
-
-            if (abs_diff <= input->_list.size())
-            {
-                diff._list[abs_diff] = true;
-                if (_debug >= DEBUG_COPIOUS)
-                    cout << "Marking entry " << abs_diff << " as represented"
-                         << endl;
-            }
-            else
-                if (_debug >= DEBUG_COPIOUS)
-                    cout << "Entry " << abs_diff << " out of bound" << endl;
-        }
-
-        if (_debug >= DEBUG_COPIOUS)
-            diff.print();
-        return diff.scan();
+      int number;
+      cin >> number;
+      current.list.push_back(number);
     }
+    list.push_back(current);
+  }
+
+  return list;
 }
 
-
-bool input(char* input_file, InputList* input)
+void output(vector<Numbers>& list)
 {
-    bool success;
-    ifstream file(input_file);
-    if (file.is_open() == false)
-    {
-        cout << "Cannot open file " << input_file << endl;
-        success = false;
-    }
-    else
-    {
-        // read the first number, which is the size of how many more integers
-        // to follow
-        int size;
-        for (int i = 0; file >> size; i++)
-        {
-            // read the sequence of integers that follow the size
-            Input current(size);
-            for (int j = 0; j < size; j++)
-                file >> current._list[j];
-            input->_list.push_back(current);
-
-
-            if (_debug >= DEBUG_LIGHT)
-            {
-                for (int j = 0; j < size; j++)
-                    cout << " " << current._list[j];
-                cout << endl;
-            }
-        }
-
-        file.close();
-        success = true;
-    }
-
-    return success;
+  for (int i = 0; i < list.size(); i++)
+    cout << (is_jolly(list[i]) ? "Jolly" : "Not jolly") << endl;
 }
 
-
-void output(InputList* input)
+int main()
 {
-    for (int i = 0; i < input->_list.size(); i++)
-    {
-        if (_debug >= DEBUG_LIGHT)
-            input->_list[i].print();
-
-        if (is_jolly(&input->_list[i]))
-            cout << "Jolly" << endl;
-        else
-            cout << "Not jolly" << endl;
-    }
-}
-
-
-int main(int argc, char** argv)
-{
-    char opt;
-    char* input_file = NULL;
-
-    while ((opt = getopt(argc, argv, "i:d:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'i':
-            input_file = optarg;
-            break;
-        case 'd':
-            _debug = atoi(optarg);
-            break;
-        default:
-            cout << "Usage: " << argv[0] << " -i input_file [-d 0|1|2]" << endl;
-        }
-    }
-
-    if (input_file == NULL || _debug < DEBUG_OFF || _debug > DEBUG_COPIOUS)
-        cout << "Usage: " << argv[0] << " -i input_file [-d 0|1|2]" << endl;
-    else
-    {
-        InputList cases;
-        if (input(input_file, &cases) != 0)
-            output(&cases);
-    }
-
-    return 0;
+  vector<Numbers> list = input();
+  output(list);
 }
